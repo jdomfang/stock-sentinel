124ba15d5c05002e591ca8c954f3b6301afacd05
Josue <kdjulianofr@hotmail.com>
2026-02-01 17:16:28 -0500

    Make utils a package to fix Streamlit import

---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+"""Local application utilities.
+
+This file makes the `utils` directory a proper Python package so imports like
+`from utils.navigation import ...` resolve to the local code (and not any
+third-party `utils` module).
+"""
