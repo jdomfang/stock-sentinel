eb265c3a1634e67ae2046a44b0c34994f8dd386e
Josue <kdjulianofr@hotmail.com>
2026-03-21 21:45:07 -0400

    Lower brand label further

--- utils/navigation.py
+++ utils/navigation.py
@@ -117,7 +117,7 @@ def render_top_nav() -> None:
           font-size: 0.78rem;
           line-height: 32px;
           white-space: nowrap;
-          margin: 31px 0 0 0;
+          margin: 48px 0 0 0;
           display: inline-block;
         }
 
