4e7dedecf06ee41649634aade31aaf1d2394de82
Josue <kdjulianofr@hotmail.com>
2026-03-23 20:40:10 -0400

    Tighten sample results gap again

--- pages/Home.py
+++ pages/Home.py
@@ -118,7 +118,7 @@ st.markdown(
       font-size: 1.35rem;
       font-weight: 800;
       letter-spacing: -0.02em;
-      margin: -0.52rem 0 0.36rem 0;
+      margin: -0.68rem 0 0.3rem 0;
     }
 
     /* How-it-works cards */
@@ -166,8 +166,8 @@ st.markdown(
     .demo-note {
       color: rgba(229,231,235,.70);
       font-size: 0.92rem;
-      margin-top: -10px;
-      margin-bottom: 4px;
+      margin-top: -11px;
+      margin-bottom: 3px;
     }
     .ticker-row {
       border: 1px solid rgba(148,163,184,0.14);
@@ -590,7 +590,7 @@ st.markdown("<div style='height: 0rem;'></div>", unsafe_allow_html=True)
 
 
 # Demo scan output (match the post-scan layout from Discovery)
-st.markdown('<div id="demo-scan" style="margin-top:-0.72rem;"></div>', unsafe_allow_html=True)
+st.markdown('<div id="demo-scan" style="margin-top:-0.95rem;"></div>', unsafe_allow_html=True)
 
 # If user clicked "View demo results" in the capability card, scroll here.
 if st.session_state.pop("_scroll_demo", False):
